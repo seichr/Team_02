2d00bc3207f03c8faad3f28d0a853d18d21ae891
Christian Seifert <christian.seifert@tugraz.at>
2021-04-14 10:42:19 +0200

    (TT-001/003) Add Base Structure and Add Func of DB [GB,SC,SS]

--- TodoTasker/app/src/main/java/com/example/todo_tasker/MainActivity.kt
+++ TodoTasker/app/src/main/java/com/example/todo_tasker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todo_tasker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +10,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+// Database To-do Structure:
+//   - UID [x]
+//   - Titel [x]
+//   - Datum [x]
+//   - [Opt] Beschreibung
+//   - [Opt] Erstellungsdatum+Zeit
+@Entity
+data class Todo(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "date") val time: Long?
+    )
+
+// Database To-do Operations
+@Dao
+interface TodoDao {
+    @Insert
+    fun insertAll(vararg todos: Todo)
+
+    @Insert
+    fun insert(vararg todo: Todo)
+
+    @Query("SELECT * FROM todo")
+    fun getAll(): List<Todo>
+
+    @Query("DELETE FROM todo")
+    fun deleteAll();
+}
+
+@Database(entities = arrayOf(Todo::class), version = 1)
+abstract class TodoDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+}
+
+
+
