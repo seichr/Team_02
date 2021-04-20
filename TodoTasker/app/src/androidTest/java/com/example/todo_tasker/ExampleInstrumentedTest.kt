f539bd9803749c9c0cb6d7821060d57c30967566
Lukas Kloiber <lukaskloiber99@gmail.com>
2021-04-20 15:57:25 +0200

    TT-001/002 Fix example test: Fixing the example test [KL,PE]

--- TodoTasker/app/src/androidTest/java/com/example/todo_tasker/ExampleInstrumentedTest.kt
+++ TodoTasker/app/src/androidTest/java/com/example/todo_tasker/ExampleInstrumentedTest.kt
@@ -7,6 +7,9 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.myapplication.MainActivity
+import junit.framework.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,7 +21,14 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SecondActivityTest {
     @Test
-    fun Example_Test() {
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.myapplication", appContext.packageName)
+    }
+
+    @Test
+    fun Our_Example_Test() {
         val activityScenario: ActivityScenario<MainActivity> =
             ActivityScenario.launch(MainActivity::class.java)
 
