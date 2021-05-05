bc23cdb24b2b36fe4c537081eee1e3b0fd340a35
Lukas Kloiber <lukaskloiber99@gmail.com>
2021-05-05 11:10:10 +0200

    [TT-10-12] Add: Add Some Database Functionality [LG,MK]

--- TodoTasker/app/src/androidTest/java/com/backend/todo_tasker/DataBaseBasicInstrumentedTest.kt
+++ TodoTasker/app/src/androidTest/java/com/backend/todo_tasker/DataBaseBasicInstrumentedTest.kt
@@ -1,4 +1,15 @@
 package com.backend.todo_tasker
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+@MediumTest
+@RunWith(AndroidJUnit4::class)
 class DataBaseBasicInstrumentedTest {
+    @Test
+    fun Test() {
+
+    }
 }
