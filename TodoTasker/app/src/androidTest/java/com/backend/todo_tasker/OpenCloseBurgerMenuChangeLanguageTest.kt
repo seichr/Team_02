661926f5b36409668cd9c355fd3db97b8d46757a
Bernd Grabner <bernd.grabner@student.tugraz.at>
2021-05-14 19:31:38 +0200

    [TT-026] push test case, we used to implement it [CS, BG]

---
+++ TodoTasker/app/src/androidTest/java/com/backend/todo_tasker/OpenCloseBurgerMenuChangeLanguageTest.kt
@@ -0,0 +1,131 @@
+package com.backend.todo_tasker
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class OpenCloseBurgerMenuChangeLanguageTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun openBurgerMenuTestPass() {
+        val appCompatImageButton = onView(
+allOf(withContentDescription("Navigate up"),
+childAtPosition(
+allOf(withId(R.id.toolbar),
+childAtPosition(
+withId(R.id.tToolbar),
+0)),
+1),
+isDisplayed()))
+        appCompatImageButton.perform(click())
+        
+        val appCompatImageButton2 = onView(
+allOf(withContentDescription("Navigate up"),
+childAtPosition(
+allOf(withId(R.id.toolbar),
+childAtPosition(
+withId(R.id.tToolbar),
+0)),
+1),
+isDisplayed()))
+        appCompatImageButton2.perform(click())
+        
+        val actionMenuItemView = onView(
+allOf(withId(R.id.language), withContentDescription("Change Language"),
+childAtPosition(
+childAtPosition(
+withId(R.id.toolbar),
+2),
+0),
+isDisplayed()))
+        actionMenuItemView.perform(click())
+        
+        val appCompatImageButton3 = onView(
+allOf(withContentDescription("Перейти вверх"),
+childAtPosition(
+allOf(withId(R.id.toolbar),
+childAtPosition(
+withId(R.id.tToolbar),
+0)),
+1),
+isDisplayed()))
+        appCompatImageButton3.perform(click())
+        
+        val actionMenuItemView2 = onView(
+allOf(withId(R.id.language), withContentDescription("Изменить язык"),
+childAtPosition(
+childAtPosition(
+withId(R.id.toolbar),
+2),
+0),
+isDisplayed()))
+        actionMenuItemView2.perform(click())
+        
+        val appCompatImageButton4 = onView(
+allOf(withContentDescription("\u200E\u200F\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200F\u200F\u200F\u200E\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200F\u200F\u200E\u200E\u200E\u200F\u200F\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200E\u200F\u200F\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200F\u200E\u200E\u200E\u200E\u200ENavigate up\u200E\u200F\u200E\u200E\u200F\u200E"),
+childAtPosition(
+allOf(withId(R.id.toolbar),
+childAtPosition(
+withId(R.id.tToolbar),
+0)),
+1),
+isDisplayed()))
+        appCompatImageButton4.perform(click())
+        
+        val appCompatImageButton5 = onView(
+allOf(withContentDescription("\u200E\u200F\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200F\u200F\u200F\u200E\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200F\u200F\u200E\u200E\u200E\u200F\u200F\u200F\u200F\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200E\u200F\u200F\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200F\u200E\u200E\u200E\u200E\u200E\u200E\u200F\u200E\u200F\u200E\u200E\u200E\u200E\u200F\u200F\u200F\u200E\u200E\u200E\u200E\u200ENavigate up\u200E\u200F\u200E\u200E\u200F\u200E"),
+childAtPosition(
+allOf(withId(R.id.toolbar),
+childAtPosition(
+withId(R.id.tToolbar),
+0)),
+1),
+isDisplayed()))
+        appCompatImageButton5.perform(click())
+        
+        val textView = onView(
+allOf(withId(R.id.text_burgermenu_title), withText("Todo-Tasker"),
+withParent(allOf(withId(R.id.layout_purple_zone),
+withParent(IsInstanceOf.instanceOf(android.widget.RelativeLayout::class.java)))),
+isDisplayed()))
+        textView.check(matches(withText("Todo-Tasker")))
+        }
+    
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+    }
