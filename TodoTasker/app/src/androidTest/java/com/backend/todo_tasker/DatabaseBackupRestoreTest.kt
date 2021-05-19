8cdbf1411eab5d7f111f0aa46b1c81d14559737d
manuelkrenn <80356900+manuelkrenn@users.noreply.github.com>
2021-05-19 10:10:23 +0200

    [TT-15] DBR now gets the correct path but not able to save file yet [MK, SS]

--- TodoTasker/app/src/androidTest/java/com/backend/todo_tasker/DatabaseBackupRestoreTest.kt
+++ TodoTasker/app/src/androidTest/java/com/backend/todo_tasker/DatabaseBackupRestoreTest.kt
@@ -1,11 +1,14 @@
 package com.backend.todo_tasker
 
+import android.Manifest
 import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
 import com.backend.todo_tasker.database.DatabaseBackupRestore
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
 
 
 @RunWith(AndroidJUnit4::class)
@@ -13,8 +16,16 @@ class DatabaseBackupRestoreTest {
 
     @Test
     fun testBackup() {
+        GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        //val backup = DatabaseBackupRestore(appContext)
-        //backup.backup()
+        val backup = DatabaseBackupRestore(appContext, null)
+        backup.backup()
+        val f= appContext.getExternalFilesDir("/todoBackup/")
+        val file = File(f,"/TodoDBBackup")
+        if(file!=null)
+            assert(file.exists())
+        else
+            assert(false)
     }
 }
--- TodoTasker/app/src/main/java/com/backend/todo_tasker/database/DatabaseBackupRestore.kt
+++ TodoTasker/app/src/main/java/com/backend/todo_tasker/database/DatabaseBackupRestore.kt
@@ -5,37 +5,64 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import java.io.*
 
 
-class DatabaseBackupRestore(Context:Context, activity: Activity?) {
+class DatabaseBackupRestore(Context: Context, activity: Activity?) {
 
     private var Activity = activity
 
     private var appContext = Context
     fun backup() {
-        verifyStoragePermissions(Activity)
+        if(Activity!= null){
+            verifyStoragePermissions(Activity)
+        }
         val db = DatabaseClass(appContext)
         val datab = db.createDb()
-        var title = (db.getLastEntry(datab).title)
+        datab.close()
+        val dbFile = appContext.getDatabasePath("todo-database").absolutePath
 
-        print(title)
+        val sdir = appContext.getExternalFilesDir("/todoBackup/")
+        if(sdir != null){
+            val sfpath =
+                    sdir.path + File.separator.toString() + "TodoDBBackup"
+                            .toString()
+            if (!sdir.exists()) {
+                sdir.mkdirs()
+            }
+            val savefile = File(sfpath)
+            val created= savefile.createNewFile()
+            val buffersize = 8 * 1024
+            val buffer = ByteArray(buffersize)
+            var bytes_read = buffersize
+            val savedb: OutputStream = FileOutputStream(sfpath)
+            val indb: InputStream = FileInputStream(dbFile)
+            while (indb.read(buffer, 0, buffersize).also({ bytes_read = it }) > 0) {
+                savedb.write(buffer, 0, bytes_read)
+            }
+            savedb.flush()
+            indb.close()
+            savedb.close()
 
+        }
     }
+
+
     fun verifyStoragePermissions(activity: Activity?) {
         val REQUEST_EXTERNAL_STORAGE = 1
         val PERMISSIONS_STORAGE =
-            arrayOf<String>( //Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
+                arrayOf<String>( //Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
         val permission = ActivityCompat.checkSelfPermission(
-            activity!!,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                activity!!,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
         if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
-                activity,
-                PERMISSIONS_STORAGE,
-                REQUEST_EXTERNAL_STORAGE
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
             )
         }
     }
